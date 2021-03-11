843c81926231756a3cc185fa91c0ce872d20e339
ulpinof <al3761456@uji.es>
2021-03-11 13:22:48 +0100

    Atributos private, constructor rellenado y metodos getters

--- src/main/java/Proyecto/Persona.java
+++ src/main/java/Proyecto/Persona.java
@@ -3,11 +3,21 @@ package Proyecto;
 import java.util.ArrayList;
 
 public class Persona {
-    String nombre, correo;
+    private String nombre, correo;
     ArrayList<Tarea> tareasResponsable;
 
-    public Persona() {
+    public Persona(String nombre, String correo) {
         super();
+        this.nombre = nombre;
+        this.correo = correo;
         tareasResponsable = new ArrayList<>();
+    }
+
+    public String getNombre () {
+        return this.nombre;
+    }
+
+    public String getCorreo() {
+        return this.correo;
     }
 }
