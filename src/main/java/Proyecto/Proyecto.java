0c3ed244889dcd0ccf25adc8ddc2d5b527228874
Robert-design <robi.tureac@gmail.com>
2021-03-17 13:47:46 +0100

    Más actualizaciones

--- src/main/java/Proyecto/Proyecto.java
+++ src/main/java/Proyecto/Proyecto.java
@@ -1,15 +1,30 @@
 package Proyecto;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Proyecto  {
+    private static String nombre;
     ArrayList<Persona> listaPersonas;
     ArrayList<Tarea> listaTareas;
 
     public Proyecto() {
-        listaPersonas = new ArrayList<Persona>();
+        listaPersonas = new ArrayList<>();
         listaTareas = new ArrayList<>();
     }
 
+    public static void setNombre() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Introduce el nombre del proyecto: ");
+        nombre = scan.next();
+    }
 
+    public static String getNombre() {
+        return nombre;
+    }
+
+    public static void main(String[] args) {
+        setNombre();
+        System.out.println(getNombre());
+    }
 }
