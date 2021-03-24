a53efd570506727698d2d6ea549b89dd5eb970b4
Robert-design <robi.tureac@gmail.com>
2021-03-24 12:17:29 +0100

    Clase menú

--- src/main/java/Proyecto/Menu.java
+++ src/main/java/Proyecto/Menu.java
@@ -1,10 +1,18 @@
 package Proyecto;
 
+import java.util.Scanner;
+
 public class Menu {
 
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
+        System.out.println("Escoge la opción");
+        Scanner scan = new Scanner(System.in)
+        int operacion =scan.nextInt();
+        switch (operacion) {
+            case 1: {
+            }
 
-
+        }
     }
 }
